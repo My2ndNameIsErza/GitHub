987ac38075314fca98767bab9d920fe5ed7e0de3
My2ndNameIsErza <57799959+My2ndNameIsErza@users.noreply.github.com>
2020-02-14 13:58:31 +0100

    pull

--- src/GitHub.java
+++ src/GitHub.java
@@ -1,5 +1,7 @@
 public class GitHub {
     public static void main(String[] args) {
-        System.out.println("first gitHub repository");
+        System.out.println("first gitHub push");
+        
+        System.out.println("second gitHub pull");
     }
 }
